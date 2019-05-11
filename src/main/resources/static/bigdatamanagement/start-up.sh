af3a066009c52196ecc48021877eefd479912b4f
yplokijuh <33217680+yplokijuh@users.noreply.github.com>
2019-05-11 18:58:37 +0800

    Add files via upload

---
+++ src/main/resources/static/bigdatamanagement/start-up.sh
@@ -0,0 +1,246 @@
+#!/bin/bash
+echo "big-data server install start up!"
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple apturl==0.5.2
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple backcall==0.1.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple beautifulsoup4==4.4.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple bleach==2.1.3
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple blinker==1.3
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Brlapi==0.6.4
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple certifi==2018.4.16
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple chardet==3.0.4
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple checkbox-support==0.22
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple click==6.7
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple command-not-found==0.3
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple cryptography==1.2.3
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple decorator==4.3.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple defer==1.0.6
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple docopt==0.6.2
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple entrypoints==0.2.3
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple feedparser==5.1.3
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Flask==1.0.2
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Flask-SocketIO==3.0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple gevent==1.3.4
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple gevent-websocket==0.10.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple greenlet==0.4.13
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple guacamole==0.9.2
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple gunicorn==19.9.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple hdfs==2.1.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple html5lib==1.0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple httplib2==0.9.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple idna==2.6
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple ipykernel==4.8.2
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple ipython==6.4.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple ipython-genutils==0.2.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple ipywidgets==7.3.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple itsdangerous==0.24
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple jedi==0.12.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Jinja2==2.10
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple jsonschema==2.6.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple jupyter==1.0.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple jupyter-client==5.2.3
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple jupyter-console==5.2.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple jupyter-core==4.4.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple kafka==1.3.5
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple language-selector==0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple louis==2.6.4
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple lxml==3.5.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Mako==1.0.3
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple MarkupSafe==1.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple mistune==0.8.3
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple nbconvert==5.3.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple nbformat==4.4.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple notebook==5.6.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple numpy==1.15.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple oauthlib==1.0.3
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple onboard==1.2.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple padme==1.1.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pandas==0.23.4
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pandocfilters==1.4.2
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple parso==0.3.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pexpect==4.6.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pickleshare==0.7.4
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Pillow==3.1.2
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple plainbox==0.25
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple prometheus-client==0.3.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple prompt-toolkit==1.0.15
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple ptyprocess==0.6.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyasn1==0.1.9
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pycups==1.9.73
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pycurl==7.43.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Pygments==2.2.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pygobject==3.20.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple PyJWT==1.3.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple PyMySQL==0.8.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyparsing==2.0.3
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple python-apt==1.1.0b1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple python-dateutil==2.7.3
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple python-debian==0.1.27
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple python-engineio==2.2.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple python-socketio==2.0.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple python-systemd==231
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pytz==2018.5
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyxdg==0.25
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyzmq==17.1.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple qtconsole==4.3.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple reportlab==3.3.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple requests==2.18.4
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple scikit-learn==0.19.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple scipy==1.1.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Send2Trash==1.5.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple sessioninstaller==0.0.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple simplegeneric==0.8.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple six==1.11.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple SQLAlchemy==1.2.11
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple ssh-import-id==5.5
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple system-service==0.3
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple terminado==0.8.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple testpath==0.3.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple tornado==5.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple traitlets==4.3.2
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple ubuntu-drivers-common==0.0.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple unattended-upgrades==0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple unity-scope-calculator==0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple unity-scope-chromiumbookmarks==0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple unity-scope-colourlovers==0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple unity-scope-devhelp==0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple unity-scope-firefoxbookmarks==0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple unity-scope-gdrive==0.7
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple unity-scope-manpages==0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple unity-scope-openclipart==0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple unity-scope-texdoc==0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple unity-scope-tomboy==0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple unity-scope-virtualbox==0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple unity-scope-yelp==0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple unity-scope-zotero==0.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple urllib3==1.22
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple usb-creator==0.3.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple wcwidth==0.1.7
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple webencodings==0.5.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Werkzeug==0.14.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple widgetsnbextension==3.3.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple xdiagnose==3.8.4.1
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple xkit==0.0.0
+
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple XlsxWriter==0.7.3
+
+ 
